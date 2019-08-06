8166b883ccf72f2d9604e3cf11b5cfbe6080944d
stevenjoro098 <stevenjoro098@gmail.com>
2019-08-06 14:48:38 +0300

    Blog

---
+++ posts/serializers.py
@@ -0,0 +1,10 @@
+__author__ = 'Da Vinci'
+from rest_framework import serializers
+from . import models
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('id', 'title', 'content', 'created_at', 'updated_at',)
+        model = models.Post
